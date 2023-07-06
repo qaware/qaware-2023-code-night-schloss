aa0860767a0fc18b6fe33fe89e680723ac465ded
Robin Kalleicher <robin.kalleicher@qaware.de>
2023-07-06 14:37:15 +0200

    Further reduce starting point for group 1

--- group1.py
+++ group1.py
@@ -16,26 +16,6 @@ def hello_world():
     return requests.get(os.environ["SERVICE_URL"] + '/hello_world').content
 
 
-def get_file_names_in_directory():
-    # TODO: Implement a method to read all file names in a directory
-    return []
-
-
-def detect_new_files():
-    # TODO: Implement an identification of new files in the given list
-    return []
-
-
-def read_from_file():
-    # TODO: Implement a method to read the content of a file
-    return None
-
-
-def generate_json():
-    # TODO: Implement a method to package information to be send to the API
-    return None
-
-
 def send_to_group2():
     # TODO: Implement the REST call to group 2
     return None
