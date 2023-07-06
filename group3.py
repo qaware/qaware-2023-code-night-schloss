e0a505726010053710d86bef8ee93450cf3ead99
Robin Kalleicher <robin.kalleicher@qaware.de>
2023-07-06 14:33:21 +0200

    Reduce starting point for group 3

--- group3.py
+++ group3.py
@@ -6,41 +6,22 @@ import requests
 # Parameter Definition: ================================================================================================
 
 
-os.environ["SERVICE_URL"] = "http://127.0.0.1:8000/"
+os.environ["SERVICE_URL"] = "http://127.0.0.1:8000"
 
 # Methods: =============================================================================================================
 
 
-def get_data(id: str):
-    return requests.get(os.environ["SERVICE_URL"] + 'sensor/' + id)
+def hello_world():
+    return requests.get(os.environ["SERVICE_URL"] + '/hello_world').content
 
 
-def list_data():
-    return requests.get(os.environ["SERVICE_URL"] + 'sensor/')
-
-
-def save_data(data: list):
-    # TODO: Implement a method to save data found in a format usable for the interface
-    return None
-
-
-def update_ui():
-    # TODO: Implement a method to update the ui according to changes
-    return None
-
-
-def generate_output():
-    # TODO: Implement a method, which formats the output
+def ask_from_group_2():
+    # TODO: Implement the REST call to group 2
     return None
 
 
 # Main Program: ========================================================================================================
 
-# TODO: generate a UI here, which then needs to be updated
-
 while True:
-    data = list_data()
-    save_data(data)
-    generate_output()
-    update_ui()
+    print(hello_world())
     time.sleep(2)
