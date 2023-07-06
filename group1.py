2c57a4531fbd6e971da2cff02283d90d924c15b7
Robin Kalleicher <robin.kalleicher@qaware.de>
2023-07-06 14:16:54 +0200

    Reduce starting point for group 1

--- group1.py
+++ group1.py
@@ -3,42 +3,41 @@ import time
 
 import requests
 
-from main import SensorDataModel, SensorUpdateModel
-
 # Parameter Definition: ================================================================================================
 
 
-os.environ["SERVICE_URL"] = "http://127.0.0.1:8000/"
+os.environ["SERVICE_URL"] = "http://127.0.0.1:8000"
 os.environ["FILESYSTEM_BASE_PATH"] = ""
 
 # Methods: =============================================================================================================
 
 
-def create_data(create: SensorDataModel):
-    return requests.post(os.environ["SERVICE_URL"] + 'sensor/', create.json())
+def hello_world():
+    return requests.get(os.environ["SERVICE_URL"] + '/hello_world').content
 
 
-def update_data(id: str, update: SensorUpdateModel):
-    return requests.put(os.environ["SERVICE_URL"] + 'sensor/' + id, update.json())
-
-
-def get_file_names_in_directory(directory_path: str):
+def get_file_names_in_directory():
     # TODO: Implement a method to read all file names in a directory
     return []
 
 
-def detect_new_files(file_names: list):
+def detect_new_files():
     # TODO: Implement an identification of new files in the given list
     return []
 
 
-def read_from_file(file_path: str):
+def read_from_file():
     # TODO: Implement a method to read the content of a file
     return None
 
 
-def generate_json(file_content: str):
+def generate_json():
     # TODO: Implement a method to package information to be send to the API
+    return None
+
+
+def send_to_group2():
+    # TODO: Implement the REST call to group 2
     return None
 
 
@@ -46,10 +45,5 @@ def generate_json(file_content: str):
 
 
 while True:
-    files = get_file_names_in_directory(os.environ["FILESYSTEM_BASE_PATH"])
-    new_files = detect_new_files(files)
-    for file in new_files:
-        content = read_from_file(os.environ["FILESYSTEM_BASE_PATH"] + file)
-        response = create_data(generate_json(content))
-        print(response.status_code)
+    print(hello_world())
     time.sleep(2)
