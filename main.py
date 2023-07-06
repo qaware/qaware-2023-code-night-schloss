f91266c422ca8a5e5c44832744265a996df40274
Robin Kalleicher <robin.kalleicher@qaware.de>
2023-07-06 10:06:06 +0200

    Extract models from main app and update requirements

--- main.py
+++ main.py
@@ -2,63 +2,13 @@ import os
 from typing import List
 
 import motor
-from bson import ObjectId
+from fastapi import FastAPI, HTTPException
 from fastapi.encoders import jsonable_encoder
 from motor import motor_asyncio
-from fastapi import FastAPI, Body, HTTPException
-from pydantic import BaseModel, Field
-from pydantic.class_validators import Optional
 from starlette import status
 from starlette.responses import JSONResponse
 
-
-class PyObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError("Invalid objectid")
-        return ObjectId(v)
-
-    @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type="string")
-
-
-class SensorDataModel(BaseModel):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    name: str = Field(...)
-    temperature: Optional[int]
-
-    class Config:
-        allow_population_by_field_name = True
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
-            "example": {
-                "name": "Sensor",
-                "temperature": 300
-            }
-        }
-
-
-class SensorUpdateModel(BaseModel):
-    name: Optional[str]
-    temperature: Optional[int]
-
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
-            "example": {
-                "name": "New Sensor Name",
-                "temperature": 320
-            }
-        }
-
+from models import SensorDataModel, SensorUpdateModel
 
 app = FastAPI()
 os.environ["MONGODB_URL"] = "mongodb://root:password@localhost:27017/?retryWrites=true&w=majority"
