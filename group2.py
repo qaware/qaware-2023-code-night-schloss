96f1536e0a21bfb7970b6afbb9dba59d4742b24a
Robin Kalleicher <robin.kalleicher@qaware.de>
2023-07-12 09:07:24 +0200

    Add address examples for group 2

--- group2.py
+++ group2.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import motor
@@ -5,6 +6,8 @@ from fastapi import FastAPI
 from motor import motor_asyncio
 from starlette import status
 from starlette.responses import JSONResponse
+
+from models import Address
 
 app = FastAPI()
 os.environ["MONGODB_URL"] = "mongodb://root:password@localhost:27017/?retryWrites=true&w=majority"
@@ -16,6 +19,20 @@ db = client.sensor_data
 def hello_world():
     response = "Hello World"
     return JSONResponse(status_code=status.HTTP_200_OK, content=response)
+
+
+@app.get("/address", response_description="Get Address")
+def get_address():
+    address = Address(country="Germany", city="Mainz", street="Leo-Trepp-Platz", house_number="1")
+    json_address = json.dumps(address.__dict__)
+    return JSONResponse(status_code=status.HTTP_200_OK, content=json_address)
+
+
+@app.post("/address", response_description="Create Address")
+def create_address(create: str):
+    address = json.loads(json.loads(create), object_hook=lambda d: Address(**d))
+    print(address.country)
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=create)
 
 
 def communicate_with_group_1():
